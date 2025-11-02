97bac67f8386b019c843d051e80d3d1b2b60ea67
ValaHardikV <valahardikv846@gmail.com>
2025-11-02 15:48:30 +0530

    Add getWatchHistory in endpoints

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -20,6 +20,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 
 import jwt from "jsonwebtoken"
 import { use } from "react";
+import mongoose from "mongoose";
 
 
 // -----------------------------------------------------------------------------
@@ -502,6 +503,66 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
 })
 
 
+const getWatchHistory = asyncHandler(async (req,res) => {
+  const user = await User.aggregate([
+
+    {
+      $match: {
+        //_id: new mongoose.Schema.Types.ObjectId(req.user._id)
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1
+                  }
+                }
+              ]
+            }
+          },
+
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner"
+              }
+            }
+          }
+        ]
+      }
+    }
+
+  ])
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(
+      200,
+      user[0].watchHistory,
+      "Watch history fetched successfully"
+    )
+  )
+})
+
+
 
 // -----------------------------------------------------------------------------
 // 🚀 EXPORT CONTROLLERS
@@ -518,5 +579,6 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCovrImage,
-  getUserChannelProfile
+  getUserChannelProfile,
+  getWatchHistory
 };
